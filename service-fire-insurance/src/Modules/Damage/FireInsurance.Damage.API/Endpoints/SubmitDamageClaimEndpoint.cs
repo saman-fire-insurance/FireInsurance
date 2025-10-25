3913d828d34d036fef31c45ccb335b037b06cfed
Faraz <Farazzvdd@gmail.com>
2025-10-25 14:30:41 +0330

    submit damage claim

---
+++ service-fire-insurance/src/Modules/Damage/FireInsurance.Damage.API/Endpoints/SubmitDamageClaimEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using Common.Abstraction.MinimalApi;
+using Common.Extensions;
+using FireInsurance.Damage.Application.UseCases.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace FireInsurance.Damage.API.Endpoints
+{
+    public sealed record SubmitDamageClaimRequest(Guid DamageClaimId);
+
+    internal sealed class SubmitDamageClaimEndpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/DamageClaim/Submit", SubmitDamageClaimAsync)
+                .RequireAuthorization()
+                .WithTags(Tags.DamageClaim);
+        }
+
+        public static async Task<IResult> SubmitDamageClaimAsync(SubmitDamageClaimRequest request, ISender sender, CancellationToken cancellationToken)
+        {
+            var command = new SubmitDamageClaimCommand(request.DamageClaimId);
+            var result = await sender.Send(command, cancellationToken);
+
+            return result.ToActionResult();
+        }
+    }
+}
